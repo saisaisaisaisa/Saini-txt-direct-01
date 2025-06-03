be38f024a00e1ed4a9d9848a06ec595b12f55971
Chiru <hasenorc@gmail.com>
2025-06-03 14:52:52 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,8 +2,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "26468828"))
-API_HASH = environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "5810663795"))
 CREDIT = "𓂃 ࣪˖ ִֶָ𝐒ɦε ۶ৎ !!"
